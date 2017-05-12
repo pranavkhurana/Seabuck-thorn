45810d60de415bb3f43c079ccd31b524978485a9
surbhi965 <surbhiagarwal965@gmail.com>
2017-05-12 15:24:20 +0530

    Update search-primer-intermediate.php

--- search-primer-intermediate.php
+++ search-primer-intermediate.php
@@ -6,6 +6,6 @@
 	if(empty($unigeneid)) die("Input a valid Unigene id.");
 	$sql='SELECT * from `table 4` WHERE "Seq ID" = '.$unigeneid;
 	$_SESSION["query"]=$sql;
-	$_SESSION["countquery"]='SELECT count(seqid) from table3 WHERE "Seq ID" = '.$unigeneid;
+	$_SESSION["countquery"]='SELECT count(seqid) from table4 WHERE "Seq ID" = '.$unigeneid;
 	header("Location: search-primer-result.php");
  ?>
