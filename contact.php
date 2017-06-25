c1de1d46c07fc9724c2d9b25c41310ef43e35b7f
surbhi agarwal <surbhiagarwal965@gmail.com>
2017-06-26 00:00:33 +0530

    added contact and intro

---
+++ contact.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>title</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://getbootstrap.com.vn/examples/equal-height-columns/equal-height-columns.css" />
+  <link rel="stylesheet" type="text/css" href="resources/css/custom-home.css">
+  <link rel="stylesheet" type="text/css" href="">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    .data{
+        padding: 15px;
+        border-radius: 10px;
+    }
+
+  </style>
+</head>
+
+<body>
+<?php include("navbar.inc.php") ?>
+
+<!-- page body container-->
+<p>
+<center><h4><b>For further enquiries please contact:</h4> 
+
+<br>Prof. P.C. Sharma<br/>
+University School of Biotechnology<br/>
+Guru Gobind Singh Indraprastha University<br/>
+Dwarka sector-16c<br/>
+New delhi<br/>
+110078    <br/>                                                                   
+Phone : +91-11-25302306<br/>
+
+Email-id   : <a href="#"> prof.pcsharma@gmail.com</a> 
+</b></center>
+</p>
+</body>
+</html>
+
+
